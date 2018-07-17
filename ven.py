0b2c343ab2f09aa266ec54ac2a15ee47bd329114
joel-g <joelaguerra1@gmail.com>
2018-07-17 16:21:04 -0700

    summarize method works

--- ven.py
+++ ven.py
@@ -6,6 +6,29 @@ def get_venmos():
     vens = requests.get("https://venmo.com/api/v5/public?limit=10")
     return vens.json()['data']
 
+def summarize(json):
+    actor = json['actor']['name']
+    target = json['transactions'][0]['target']['name']
+    method = json['type']
+    message = json['message']
+    if method == 'payment':
+        method = 'paid'
+    elif method == 'charge':
+        method = 'charged'
+    return actor + " " + method + " " + target + " for " + message
+
 vens = get_venmos()
 
-print(vens[0]['actor']['name'])
+actor = vens[0]['actor']['name']
+
+print(actor)
+
+target = vens[0]['transactions'][0]['target']['name']
+
+print(target)
+
+message = vens[0]['message']
+
+print(message)
+
+print(summarize(vens[0]))
