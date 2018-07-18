f3c95d9099c8c018b27e77e8c22f478515f6f111
joel-g <joelaguerra1@gmail.com>
2018-07-17 17:17:12 -0700

    working

--- ven.py
+++ ven.py
@@ -1,9 +1,22 @@
-import requests
+import requests, tweepy, time
 
+with open('config.ini','r') as config:
+  tokens = config.readlines()
+  TW_CONSUMER_KEY = tokens[0].rstrip()
+  TW_CONSUMER_SECRET = tokens[1].rstrip()
+  TW_ACCESS_KEY = tokens[2].rstrip()
+  TW_ACCESS_SECRET = tokens[3].rstrip()
 
+def authenticate_twitter():
+  print('Authenticating twitter...')
+  auth = tweepy.OAuthHandler(TW_CONSUMER_KEY, TW_CONSUMER_SECRET)
+  auth.set_access_token(TW_ACCESS_KEY, TW_ACCESS_SECRET)
+  twitter = tweepy.API(auth)
+  print('Twitter authenticated.\n')
+  return twitter
 
-def get_venmos():
-    vens = requests.get("https://venmo.com/api/v5/public?limit=1000")
+def get_venmos(num):
+    vens = requests.get("https://venmo.com/api/v5/public?limit=" + str(num))
     return vens.json()['data']
 
 def summarize(json):
@@ -17,14 +30,30 @@ def summarize(json):
         method = 'charged'
     return actor + " " + method + " " + target + " for " + message
 
-
-def check_ven(json):
-    if any(x in json['message'].lower() for x in ('weed', 'meth', 'heroin', 'booze', 'sex', 'alcohol', 'pills', 'drank', 'marijuana', 'coke', 'cocaine')):
+def tweet(twitter, ven):
+    summary = summarize(ven)
+    if 'payment' == ven['type']:
+        photo = ven['actor']['picture']
+    elif 'charge' == ven['type']:
+        photo = ven['transactions'][0]['target']['picture']
+    twitter.update_status(summary + " \n" + photo)
+    
+def check_for_drugs(json):
+    if any(x in json['message'].lower() for x in ('💉', '💊', 'weed', 'meth', 'heroin', 'drugs', 'sex', 'alcohol', 'pills',  'marijuana', 'coke', 'cocaine', 'blowjob', 'porn', 'booze', 'drank')):
         return json['message']
     else: 
         return False
 
 
-for ven in vens:
-    if check_ven(ven):
-        print(summarize(ven))
+def main():
+    twitter = authenticate_twitter()
+    while True:
+        vens = get_venmos(1000)
+        for ven in vens:
+            if check_for_drugs(ven):
+                tweet(twitter, ven)
+                print("Tweeted " + summarize(ven) + "\n Sleeping for an hour")
+                time.sleep(3600)
+
+if __name__ == '__main__':
+  main()
